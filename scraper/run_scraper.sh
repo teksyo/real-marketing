c1e1133e61772927d543a8f7dd38233de5dcbea9
Haseeb Javed <haseeb.javed@teksyo.com>
2025-06-13 18:16:18 +0500

    upadated sh file

--- scraper/run_scraper.sh
+++ scraper/run_scraper.sh
@@ -2,8 +2,8 @@
 
 echo "[$(date)] Starting script"
 
-npx prisma generate && \
-npx prisma migrate deploy && \
+prisma generate && \
+prisma migrate deploy && \
 python zillow_complete_scraper.py
 
 echo "[$(date)] Script finished with code $?"
