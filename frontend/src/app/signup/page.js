e3c186ab9c3670434b608b2a3428629e21adad08
Haseeb Javed <haseeb.javed@teksyo.com>
2025-06-13 17:36:00 +0500

    dropdown border color applied for region in registration page

--- frontend/src/app/signup/page.js
+++ frontend/src/app/signup/page.js
@@ -97,11 +97,12 @@ export default function SignUp() {
                 Region
               </label>
               <select
-                className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm rounded-md"
+                className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-1 border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm rounded-md"
                 value={formData.region}
                 onChange={(e) =>
                   setFormData({ ...formData, region: e.target.value })
                 }
+                style={{ border: "1px solid", borderColor: "rgb(209 213 219)" }}
               >
                 <option value="NO">-- Not Assigned --</option>
                 <option value="US">United States</option>
